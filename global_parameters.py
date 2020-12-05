c9e56782cdc633811722fea80dea896eba901146
Alibek Utyubayev <uas91@hotmail.com>
2020-12-06 01:56:33 +0600

    chores: adding global parameters file

---
+++ global_parameters.py
@@ -0,0 +1,5 @@
+global_number_movies_returned  = 12
+global_database_filepath = 'recommendation_database/movie_recs_2000_multiply_genres_12.db'
+global_recs_table_name = 'Closest_movies'
+global_min_movies_rated_by_user = 2
+global_min_users_rated_movie = 80
